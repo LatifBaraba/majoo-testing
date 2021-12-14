b02f35279de278fc3920fb42cae2834b157fdcd4
Latif <latifbaraba88@gmail.com>
2021-12-14 22:53:45 +0700

    add, delete

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Header from '../layouts/header'
 import Banner from '../layouts/banner'
-import { fetchTodo } from '../store'
+import { fetchTodo, addTodo, deleteTodo } from '../store'
 
 const Home = () => {
     const dispatch = useDispatch()
-    const { todoActive, todoUnActive, loading, error } = useSelector((state) => state.todo)
-    console.log(todoActive, todoUnActive, loading, error)
+    const { todoActive, todoUnActive, loading, error } = useSelector(state => state.todo)
+
+    const [input, setInput] = useState()
 
     useEffect(() => {
         dispatch(fetchTodo())
@@ -21,10 +22,10 @@ const Home = () => {
             <section className='section-todo'>
                 <div className='row justify-content-center g-2'>
                     <div className='col-md-6'>
-                        <input type='email' className='form-control' placeholder='new todo' />
+                        <input type='email' className='form-control' placeholder='new todo' onChange={(e) => setInput(e.target.value)}/>
                     </div>
                     <div className='col-md-2'>
-                        <button type='button' className='btn btn-outline-success'>
+                        <button type='button' className='btn btn-outline-success' onClick={() => dispatch(addTodo(input))}>
                             Add Todo
                         </button>
                     </div>
@@ -35,16 +36,38 @@ const Home = () => {
                 <div className='row my-3'>
                     <h2>Active Todo</h2>
                     <div className='active-todo'>
-                        <div className='todo'>
-                            <h3>todo satu</h3>
+                        <div className='row g-4'>
+                            {todoActive ? (
+                                todoActive.map((value, idx) => (
+                                    <div className='col-12 todo d-flex flex-row' key={idx}>
+                                        <h2>{value.title}</h2>
+                                        <button type='button' className='btn btn-outline-success ms-auto' onClick={() => dispatch(deleteTodo(value))}>
+                                            delete
+                                        </button>
+                                    </div>
+                                ))
+                            ) : (
+                                <span>Loading</span>
+                            )}
                         </div>
                     </div>
                 </div>
                 <div className='row'>
                     <h2>Un-active Todo</h2>
+                    <div className='filter'>
+                        <span>Asc</span>
+                    </div>
                     <div className='active-todo'>
-                        <div className='todo'>
-                            <h3>todo dua</h3>
+                        <div className='row g-4'>
+                            {todoUnActive ? (
+                                todoUnActive.map((value, idx) => (
+                                    <div className='col-12 todo' key={idx}>
+                                        <h2>{value.title}</h2>
+                                    </div>
+                                ))
+                            ) : (
+                                <span>Loading</span>
+                            )}
                         </div>
                     </div>
                 </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
 
 const initialState = {
     loading: false,
@@ -19,8 +20,8 @@ export const todoSlice = createSlice({
             let active = []
             let inactive = []
             payload.data.forEach(value => {
-              value.status === 0 ? active.push(value) : inactive.push(value)
-            });
+                value.status === 0 ? active.push(value) : inactive.push(value)
+            })
             state.todoActive = active
             state.todoUnActive = inactive
             state.loading = false
@@ -30,10 +31,24 @@ export const todoSlice = createSlice({
             state.loading = false
             state.error = true
         },
+        addTodo: (state, { payload }) => {
+            const data = {
+                createdAt: new Date(),
+                description: 'lorem ipsum',
+                id: uuidv4(),
+                status: 0,
+                title: payload,
+            }
+            state.todoActive.push(data)
+        },
+        deleteTodo: (state, { payload }) => {
+            state.todoUnActive.push(payload)
+            state.todoActive.pop()
+        },
     },
 })
 
-export const { getTodo, getTodoFailure, getTodoSuccess } = todoSlice.actions
+export const { getTodo, getTodoFailure, getTodoSuccess, addTodo, deleteTodo } = todoSlice.actions
 
 export default todoSlice.reducer
 
@@ -44,7 +59,6 @@ export function fetchTodo() {
         try {
             const response = await axios('https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list')
             const data = await response
-            console.log(data, response)
             dispatch(getTodoSuccess(data))
         } catch (error) {
             dispatch(getTodoFailure())
